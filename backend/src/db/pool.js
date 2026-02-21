5e9c476d772446ab4a14bbef38b3677a36aa7485
Aniruddh-pixel <aniruddharao16@gmail.com>
2026-02-21 22:44:21 +0530

    Configure Postgres using DATABASE_URL for Render

--- backend/src/db/pool.js
+++ backend/src/db/pool.js
@@ -1,16 +1,15 @@
-// backend\src\db\pool.js
 import dotenv from "dotenv";
-dotenv.config(); // 🔴 MUST be here (before Pool)
+dotenv.config();
 
 import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: String(process.env.DB_PASSWORD), // 🔴 force string
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
 export default pool;
